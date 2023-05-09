d5b543cfa56f2496b826cddb2751903b6d9c33d2
Bhairav819T <131357921+Bhairav819T@users.noreply.github.com>
2023-05-09 14:48:03 +0400

    Create j4.java

---
+++ j4.java
@@ -0,0 +1,10 @@
+public class j4{
+  public static void main (string [] args)
+  {
+    int a,b,ans;
+    a=10;
+    b=20;
+    ans=a+b;
+    system.out.print("addition of two number is"+ans);
+  }
+}
